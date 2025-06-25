b1df89837ffb67753b59f3e6efe5eb4a4fa9f950
Amelia Teodora Gherdan <ameliagherdan@gmail.com>
2025-06-25 13:01:25 +0300

    Initial commit: Java Spring Boot product service

---
+++ src/main/java/com/amelia/codes/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amelia.codes.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+}
